5fe34d996e40684d9e5b13872fd36ab01fe57681
dev-ro <dev.rohit256@gmail.com>
2020-11-01 20:41:08 +0530

    added team model & migration

---
+++ database/migrations/2020_11_01_150530_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('team_name', 100);
+            $table->string('designation' , 80);
+            $table->text('image' , 80);
+            $table->text('description')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,5 +3,157 @@
     @include('ui::meta')
 @endsection
 @section('content')
+<section class="section service-area ptb_150">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+                <!-- Single Service -->
+                <div class="single-service service-gallery m-0 overflow-hidden">
+                    <!-- Service Thumb -->
+                    <div class="service-thumb">
+                        <a href="#"><img src="assets/img/case_studies/case_studies_1/thumb_1.jpg" alt=""></a>
+                    </div>
+                    <!-- Service Content -->
+                    <div class="service-content bg-white">
+                        <a href="#">
+                            <h3>Data Analytics</h3>
+                        </a>
+                        <p class="py-3">Lorem ipsum dolor sit amet, consectet ur adipisicing elit.</p>
+                        <a class="service-btn" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <!-- Single Service -->
+                <div class="single-service service-gallery m-0 overflow-hidden">
+                    <!-- Service Thumb -->
+                    <div class="service-thumb">
+                        <a href="#"><img src="assets/img/case_studies/case_studies_1/thumb_2.jpg" alt=""></a>
+                    </div>
+                    <!-- Service Content -->
+                    <div class="service-content bg-white">
+                        <a href="#">
+                            <h3>Website Growth</h3>
+                        </a>
+                        <p class="py-3">Lorem ipsum dolor sit amet, consectet ur adipisicing elit.</p>
+                        <a class="service-btn" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <!-- Single Service -->
+                <div class="single-service service-gallery m-0 overflow-hidden">
+                    <!-- Service Thumb -->
+                    <div class="service-thumb">
+                        <a href="#"><img src="assets/img/case_studies/case_studies_1/thumb_3.jpg" alt=""></a>
+                    </div>
+                    <!-- Service Content -->
+                    <div class="service-content bg-white">
+                        <a href="#">
+                            <h3>Seo Ranking</h3>
+                        </a>
+                        <p class="py-3">Lorem ipsum dolor sit amet, consectet ur adipisicing elit.</p>
+                        <a class="service-btn" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @include('ui::growth')
+<section class="section content-area ptb_150">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-12 col-lg-6">
+                <!-- Profile Circle Wrapper -->
+                <div class="profile-circle-wrapper circle-animation d-none d-sm-block">
+                    <!-- Profile Inner -->
+                    <div class="profile-inner">
+                        <!-- Profile Circle -->
+                        <div class="profile-circle circle-lg">
+                            <span class="profile-icon icon-1">
+                                <img class="icon-1-img" src="assets/img/content/profile-icons/profile_icon_1.svg" alt="" />
+                            </span>
+                            <span class="profile-icon icon-2">
+                                <img class="icon-2-img" src="assets/img/content/profile-icons/profile_icon_2.svg" alt="" />
+                            </span>
+                            <span class="profile-icon icon-3">
+                                <img class="icon-3-img" src="assets/img/content/profile-icons/profile_icon_1.svg" alt="" />
+                            </span>
+                            <span class="profile-icon icon-4">
+                                <img class="icon-4-img" src="assets/img/content/profile-icons/profile_icon_2.svg" alt="" />
+                            </span>
+                        </div>
+
+                        <!-- Profile Circle -->
+                        <div class="profile-circle circle-md">
+                            <span class="profile-icon icon-5">
+                                <img class="icon-5-img" src="assets/img/content/profile-icons/profile_icon_3.svg" alt="" />
+                            </span>
+                            <span class="profile-icon icon-6">
+                                <img class="icon-6-img" src="assets/img/content/profile-icons/profile_icon_3.svg" alt="" />
+                            </span>
+                            <span class="profile-icon icon-7">
+                                <img class="icon-7-img" src="assets/img/content/profile-icons/profile_icon_3.svg" alt="" />
+                            </span>
+                        </div>
+
+                        <!-- Profile Circle -->
+                        <div class="profile-circle circle-sm">
+                            <span class="profile-icon icon-8">
+                                <img class="icon-8-img" src="assets/img/content/profile-icons/profile_icon_4.svg" alt="" />
+                            </span>
+                            <span class="profile-icon icon-9">
+                                <img class="icon-9-img" src="assets/img/content/profile-icons/profile_icon_4.svg" alt="" />
+                            </span>
+                        </div>
+                    </div>
+                    <img class="folder-img" src="assets/img/content/folders.png" alt="" />
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <!-- Content Inner -->
+                <div class="content-inner text-center pt-sm-4 pt-lg-0 mt-sm-5 mt-lg-0">
+                    <!-- Section Heading -->
+                    <div class="section-heading text-center mb-3">
+                        <h2>Work smarter,<br> not harder.</h2>
+                        <p class="d-none d-sm-block mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati dignissimos quae quo ad iste ipsum officiis deleniti asperiores sit.</p>
+                        <p class="d-block d-sm-none mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati.</p>
+                    </div>
+                    <!-- Content List -->
+                    <ul class="content-list text-left">
+                        <!-- Single Content List -->
+                        <li class="single-content-list media py-2">
+                            <div class="content-icon pr-4">
+                                <span class="color-2"><i class="fas fa-angle-double-right"></i></span>
+                            </div>
+                            <div class="content-text media-body">
+                                <span><b>Digital Agency &amp; Marketing</b><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio.</span>
+                            </div>
+                        </li>
+                        <!-- Single Content List -->
+                        <li class="single-content-list media py-2">
+                            <div class="content-icon pr-4">
+                                <span class="color-2"><i class="fas fa-angle-double-right"></i></span>
+                            </div>
+                            <div class="content-text media-body">
+                                <span><b>Planning To Startup</b><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio.</span>
+                            </div>
+                        </li>
+                        <!-- Single Content List -->
+                        <li class="single-content-list media py-2">
+                            <div class="content-icon pr-4">
+                                <span class="color-2"><i class="fas fa-angle-double-right"></i></span>
+                            </div>
+                            <div class="content-text media-body">
+                                <span><b>Content Management</b><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio.</span>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="#" class="btn btn-bordered mt-4">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
