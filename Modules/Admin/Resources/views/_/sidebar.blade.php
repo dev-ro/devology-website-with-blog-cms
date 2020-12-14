967397fa0f15d6f4e374c19271f67b91f979d0e8
dev-ro <dev.rohit256@gmail.com>
2020-12-14 12:12:31 +0530

    added logout option

--- Modules/Admin/Resources/views/_/sidebar.blade.php
+++ Modules/Admin/Resources/views/_/sidebar.blade.php
@@ -156,6 +156,17 @@
             </li>
           </ul>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+            <i class="far fa-circle nav-icon"></i>
+            <p>{{ __('Logout') }}</p> 
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+          </form>
+        </li>
       </ul>
     </nav>
   </div>
